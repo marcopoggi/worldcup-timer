0f6a27773450976360c4f4a8311b545b6a1a5214
Marco Poggi <marcopoggi2017@gmail.com>
2021-11-29 07:48:57 -0300

    add localStorage obj / call initLang & langButton functions

--- src/js/init.js
+++ src/js/init.js
@@ -1,6 +1,10 @@
 import { timer,elems } from "./handlers/view.js";
+import { initLang,langButton } from "./handlers/lang.js";
 
 
 export const init = () => {
+    let storage = window.localStorage;
     timer(elems);
+    initLang(storage);
+    langButton(storage);
 }
