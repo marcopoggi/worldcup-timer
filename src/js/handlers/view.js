7f14bee4d4e200e0431e42803f209227f575a07e
Marco Poggi <marcopoggi2017@gmail.com>
2021-12-16 17:45:17 -0300

    status added to know completion and the different cases

--- src/js/handlers/view.js
+++ src/js/handlers/view.js
@@ -1,28 +1,43 @@
 import { getTime } from "./time.js"
 
-export const elems = {
+const elems = {
     day: document.getElementById('day-text'),
     hour: document.getElementById('hour-text'),
     min: document.getElementById('min-text'),
     sec: document.getElementById('sec-text'),
 }
 
-export const timer = elements => {
-    updateView(elements);
-    setInterval(()=>{
-        updateView(elements);
-    },1000)
+export const timer = (state,endDate = '00-00-00') => {
+    console.log(state);
+    if(state === true){
+        let id = setInterval(()=>{
+            console.log('dentro')
+            updateView(elems,id,endDate);
+        },1000)
+    }
+    else setView({},elems,true);
 }
 
-const setView = (data,elements) =>{
+const setView = (data,elements,finish = false) =>{
+    if(finish){
+        let mainContent = document.getElementById("clock-container"),
+        finish = document.getElementById("finish-timer");
+        mainContent.style.display = 'none';
+        finish.style.display = 'block'
+        return
+    }
     elements.day.textContent = data.d;
     elements.hour.textContent = data.h;
     elements.min.textContent = data.m;
     elements.sec.textContent = data.s;
 }
 
-const updateView = (elements) => {
-    let data = getTime();
-    setView(data,elements)
+const updateView = (elements, intervalID = false,endDate) => {
+    let data = getTime(endDate);
+    if(data.d =='00' && data.h =='00' && data.m =='00' && data.s =='00'){ //finish
+        setView(data,elements,true);
+        clearInterval(intervalID);
+    }
+    setView(data,elements);
 }
 
