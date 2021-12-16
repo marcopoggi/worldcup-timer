692d1e3ffce6df6f5fc89949c0eb81c97e45cefc
Marco Poggi <marcopoggi2017@gmail.com>
2021-12-16 17:31:28 -0300

    call to getData / I compare end date and today's date and start the timer

--- src/js/init.js
+++ src/js/init.js
@@ -1,10 +1,17 @@
-import { timer,elems } from "./handlers/view.js";
+import { timer } from "./handlers/view.js";
 import { initLang,langButton } from "./handlers/lang.js";
+import { getData } from "./services/request.js";
 
 
 export const init = () => {
     let storage = window.localStorage;
-    timer(elems);
+    let settings = getData('../../src/assets/resources/json/settings.json')
+    settings.then( res => {
+        const end = new Date(res["end-date"]).getTime() + (new Date().getTimezoneOffset() * 60000);
+        let today = new Date().getTime();
+        
+        (end > today) ? timer(true,end) : timer(false);
+    })
     initLang(storage);
     langButton(storage);
 }
